b443779bd30503dd70436683b0316198b1ad745d
cmcarhtur5 <crobertmcarthur@gmail.com>
2023-04-06 22:25:46 -0600

    Created PackTracker
    Created the PackTracker VisualStudio Project.

---
+++ PackTracker/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PackTracker;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
