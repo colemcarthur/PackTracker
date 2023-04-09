0a31c6abd0b4790fb0c82e73fddc3eda72a94422
krookdfinger <59386088+krookdfinger@users.noreply.github.com>
2023-04-08 23:13:23 -0600

    Updates to Android

--- PackTracker/MVVM/Views/QRCodePageView.xaml.cs
+++ PackTracker/MVVM/Views/QRCodePageView.xaml.cs
@@ -1,14 +1,15 @@
-﻿using Microsoft.Maui.Platform;
-using Microsoft.Maui.Graphics.Platform;
-using Microsoft.Maui.Controls;
-using Microsoft.Maui.Graphics;
-using Microsoft.Maui.Controls.PlatformConfiguration;
-using Microsoft.Maui.Controls.Compatibility.Platform;
+﻿//using Microsoft.Maui.Platform;
+//using Microsoft.Maui.Graphics.Platform;
+//using Microsoft.Maui.Controls;
+//using Microsoft.Maui.Graphics;
+//using Microsoft.Maui.Controls.PlatformConfiguration;
+//using Microsoft.Maui.Controls.Compatibility.Platform;
 
 using ZXing.Net.Maui;
-using ZXing.QrCode;
-using ZXing.QrCode.Internal;
-using ZXing.Common;
+//using ZXing.QrCode;
+//using ZXing.QrCode.Internal;
+//using ZXing.Common;
+
 using PackTracker.Platforms;
 
 namespace PackTracker.MVVM.Views;
@@ -16,13 +17,20 @@ namespace PackTracker.MVVM.Views;
 public partial class QRCodePageView : ContentPage
 {
 
+	Image testImage { get; set; }
+
 	public QRCodePageView()
 	{
 		InitializeComponent();
 
 		Stream sr = App.BarcodeService.ConvertImageStream("Hell", 400, 400);
-        
-	}
+
+        Image image = new Image
+        {
+            Source = ImageSource.FromStream(() => sr)
+        };
+        testImage = image;
+    }
 
 }
 
